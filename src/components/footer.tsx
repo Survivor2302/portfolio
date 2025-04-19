550ac4c0cdfad46ce6e4fe33d42954a08d77c1b8
Léo Bournizien <73833842+Survivor2302@users.noreply.github.com>
2025-04-19 11:15:43 +0200

    footer link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,8 +5,10 @@ export default function Footer() {
     <div className="w-full px-16 py-2 border-t border-black  bg-white">
       <div className="flex justify-between">
         <div className="flex space-x-4">
-          <Link href="">LinkedIn</Link>
-          <Link href="">Github</Link>
+          <Link href="https://www.linkedin.com/in/leo-bournizien-0a75a3209/">
+            LinkedIn
+          </Link>
+          <Link href="https://github.com/Survivor2302">Github</Link>
         </div>
         ©{new Date().getFullYear()} Léo Bournizien - Tous droits réservés
       </div>
